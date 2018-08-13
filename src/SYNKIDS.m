cc54fd4941fd9e45b5f99e58c5cca726f8e09f17
Sam Habiel <Sam.Habiel@gmail.com>
2018-08-13 11:45:51 -0400

    Fix some tiny XINDEX errors.

--- src/SYNKIDS.m
+++ src/SYNKIDS.m
@@ -1,4 +1,4 @@
-SYNKIDS	;ven/gpl - fhir loader utilities ; 2/20/18 4:11am
+SYNKIDS	;ven/gpl - fhir loader utilities ;2018-08-03  11:24 AM
 	;;1.0;fhirloader;;oct 19, 2017;Build 2
 	;
 	; Authored by George P. Lilly 2017-2018
@@ -9,24 +9,24 @@ PRETRAN ;
  n root s root=$$setroot^%wd("kids-manifest") ; tells us what to load
  i '$d(@root@("load")) q  ; nothing to load
  n zi s zi=""
- f  s zi=$o(@root@("load",zi)) q:zi=""  d  ;
+ f  s zi=$o(@root@("load",zi)) q:zi=""  d
  . n gn s gn=$$setroot^%wd(zi)
  . q:gn=""
  . m @XPDGREF@(zi)=@gn
  m @XPDGREF@("kids-manifest")=@root
- q 
+ q:$q "" q
  ;
 PRE ; load the transported graphs
  n zmani s zmani=$na(@XPDGREF@("kids-manifest"))
- i '$d(@zmani) q  ;
+ i '$d(@zmani) q
  n zi s zi=""
- f  s zi=$o(@zmani@("load",zi)) q:zi=""  d  
+ f  s zi=$o(@zmani@("load",zi)) q:zi=""  d
  . n gn s gn=$$setroot^%wd(zi)
  . i $d(@gn) d purgegraph^%wd(zi)
  . m @gn=@XPDGREF@(zi)
  q
  ;
-schedload(graph)
+schedload(graph) ;
  n root s root=$$setroot^%wd("kids-manifest")
  i +$$rootOf^%wd(graph)=-1 d  q  ; can't load that; doesn't exist
  . w !,"error, graph doesn't exist: "_graph
@@ -45,5 +45,3 @@ schedlab ; schedule the loading of the loinc-lab-map graph
  d schedload("loinc-lab-map")
  q
  ;
-
- 
