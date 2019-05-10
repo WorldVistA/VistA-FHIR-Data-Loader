30753aea46b8da0841ce3f9129dcd84e09755b7e
Sam Habiel <Sam.Habiel@gmail.com>
2019-05-10 10:38:54 -0400

    Removed old unused mapping routines; update current one.

--- src/SYNDHPMAPO.m
+++
@@ -1,9 +0,0 @@
-SYNDHPMAPO ; terminology mapping
- ;;0.2;VISTA SYN DATA LOADER;;Feb 07, 2019
- ;
- ; under construction
-ALLRGY() ;
- S D=" (disorder)"
- S N=""
- F  S N=$O(^ZZFJFALL(N)) Q:N=""  D
- .
