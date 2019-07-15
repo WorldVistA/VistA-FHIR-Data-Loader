f9b510ff9288166ac94c92bfba311264a5713074
George Lilly <glilly@glilly.net>
2019-07-15 22:08:04 +0000

    convert CarePlans to load out of graph instead of symbol table

--- src/SYNFCP.m
+++ src/SYNFCP.m
@@ -11,14 +11,10 @@ importCarePlan(rtn,ien,args)  ; entry point for loading Careplans for a patient
  n grtn
  n root s root=$$setroot^SYNWD("fhir-intake")
  d wsIntakeCareplan(.args,,.grtn,ien)
- i $d(grtn) d  ; something was returned
- . k @root@(ien,"load","careplan")
- . m @root@(ien,"load","careplan")=grtn("careplan")
- . if $g(args("debug"))=1 m rtn=grtn
+ ;
  s rtn("careplanStatus","status")=$g(grtn("status","status"))
  s rtn("careplanStatus","loaded")=$g(grtn("status","loaded"))
  s rtn("careplanStatus","errors")=$g(grtn("status","errors"))
- ;b
  ;
  ;
  q
@@ -28,21 +24,21 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  ; result is json and summarizes what was done
  ; args include patientId
  ; ien is specified for internal calls, where the json is already in a graph
+ ;
+ n root,troot
+ s root=$$setroot^SYNWD("fhir-intake")
+ ;
  n jtmp,json,jrslt,eval
- ;i $g(ien)'="" if $$loadStatus("careplan","",ien)=1 d  q  ;
- ;. s result("careplanStatus","status")="alreadyLoaded"
  i $g(ien)'="" d  ; internal call
- . d getIntakeFhir^SYNFHIR("json",,"CarePlan",ien,1)
- e  d  ;
- . s args("load")=0
- . merge jtmp=BODY
- . do decode^SYNJSONE("jtmp","json")
- i '$d(json) q  ;
- m ^gpl("gjson")=json
+ . s troot=$na(@root@(ien,"type","CarePlan"))
+ . s eval=$na(@root@(ien,"load")) ; move eval to the graph
+ e  q 0  ; sending not decoded json in BODY to this routine not supported
+ ; todo: locate the patient and add the encounters in BODY to the graph
+ s json=$na(@root@(ien,"json"))
  ;
  ; determine the patient
  ;
- n dfn,eval
+ n dfn
  if $g(ien)'="" d  ;
  . s dfn=$$ien2dfn^SYNFUTL(ien) ; look up dfn in the graph
  else  d  ;
@@ -55,21 +51,21 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  ;
  ;
  new zi s zi=0
- for  set zi=$order(json("entry",zi)) quit:+zi=0  do  ;
+ for  set zi=$order(@troot@(zi)) quit:+zi=0  do  ;
  . ;
  . ; define a place to log the processing of this entry
  . ;
- . new jlog set jlog=$name(eval("careplan",zi))
+ . new jlog set jlog=$name(@eval@("careplan",zi))
  . ;
  . d log(jlog,"DFN: "_dfn)
  . ;
  . ; insure that the resourceType is CarePlan
  . ;
- . new type set type=$get(json("entry",zi,"resource","resourceType"))
+ . new type set type=$get(@json@("entry",zi,"resource","resourceType"))
  . if type'="CarePlan" do  quit  ;
- . . set eval("careplan",zi,"vars","resourceType")=type
+ . . set @eval@("careplan",zi,"vars","resourceType")=type
  . . do log(jlog,"Resource type not CarePlan, skipping entry")
- . set eval("careplan",zi,"vars","resourceType")=type
+ . set @eval@("careplan",zi,"vars","resourceType")=type
  . ;
  . ; see if this resource has already been loaded. if so, skip it
  . ;
@@ -81,34 +77,34 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . ;
  . ; determine the id of the resource
  . ;
- . new id set id=$get(json("entry",zi,"resource","id"))
- . set eval("careplan",zi,"vars","id")=id
+ . new id set id=$get(@json@("entry",zi,"resource","id"))
+ . set @eval@("careplan",zi,"vars","id")=id
  . d log(jlog,"ID is: "_id)
  . ;
  . ;
  . ; determine the encounter visit ien
  . n encounterId
- . s encounterId=$g(json("entry",zi,"resource","context","reference"))
- . i encounterId="" s encounterId=$g(json("entry",zi,"resource","encounter","reference"))
+ . s encounterId=$g(@json@("entry",zi,"resource","context","reference"))
+ . i encounterId="" s encounterId=$g(@json@("entry",zi,"resource","encounter","reference"))
  . ;i encounterId["urn:uuid:" s encounterId=$p(encounterId,"urn:uuid:",2)
- . s eval("careplan",zi,"vars","encounterId")=encounterId
+ . s @eval@("careplan",zi,"vars","encounterId")=encounterId
  . d log(jlog,"reference encounter ID is : "_encounterId)
  . ;
  . ; determine visit ien
  . ;
  . n visitIen s visitIen=$$visitIen^SYNFENC(ien,encounterId)
- . s eval("careplan",zi,"vars","visitIen")=visitIen
+ . s @eval@("careplan",zi,"vars","visitIen")=visitIen
  . d log(jlog,"visit ien is: "_visitIen)
  . n visitDate s visitDate=$$GET1^DIQ(9000010,visitIen_",",.01,"I")
  . i visitDate'=-1 d  ;
- . . s eval("careplan",zi,"vars","visitDate")=visitDate
+ . . s @eval@("careplan",zi,"vars","visitDate")=visitDate
  . . d log(jlog,"visit date is: "_visitDate)
  . e  d log(jlog,"visit date unknow")
  . ;
  . ; determine the category code
  . ;
- . new sctcode set sctcode=$get(json("entry",zi,"resource","category",1,"coding",1,"code"))
- . n cattext s cattext=$get(json("entry",zi,"resource","category",1,"coding",1,"display"))
+ . new sctcode set sctcode=$get(@json@("entry",zi,"resource","category",1,"coding",1,"code"))
+ . n cattext s cattext=$get(@json@("entry",zi,"resource","category",1,"coding",1,"display"))
  . n x s $p(x,"-",80)=""
  . do TONOTE^SYNFTIU(ien,encounterId,x)
  . do TONOTE^SYNFTIU(ien,encounterId,"Care Plan Instituted")
@@ -117,8 +113,8 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . do TONOTE^SYNFTIU(ien,encounterId,"  "_cattext_" (SCT:"_sctcode_")")
  . do log(jlog,"category code is: "_sctcode)
  . do log(jlog,"category text is: "_cattext)
- . set eval("careplan",zi,"vars","code")=sctcode
- . set eval("careplan",zi,"vars","categoryText")=cattext
+ . set @eval@("careplan",zi,"vars","code")=sctcode
+ . set @eval@("careplan",zi,"vars","categoryText")=cattext
  . d log(jlog,"Care Plan Category Code: "_sctcode)
  . d log(jlog,"Care Plan Category Text: "_cattext)
  . n hfcat,hfcatien
@@ -132,9 +128,9 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . ; determine the "addresses" code
  . ;
  . n addcode s addcode=""
- . i $g(json("entry",zi,"resource","addresses",1,"reference"))'="" d  ;
+ . i $g(@json@("entry",zi,"resource","addresses",1,"reference"))'="" d  ;
  . . n condref
- . . s condref=$g(json("entry",zi,"resource","addresses",1,"reference"))
+ . . s condref=$g(@json@("entry",zi,"resource","addresses",1,"reference"))
  . . q:condref=""
  . . d log(jlog,"Addresses Condition Reference: "_condref)
  . . s addcode=$$DX^SYNFCP(ien,condref)
@@ -156,40 +152,40 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . ;d:notmapped MAPERR^SYNQLDM(sctcode,icdcodetype)
  . ;do log(jlog,"icd mapping is: "_icdcode)
  . ;do:notmapped log(jlog,"snomed code "_sctcode_" is not mapped")
- . ;set eval("careplan",zi,"vars","mappedIcdCode")=icdcode
+ . ;set @eval@("careplan",zi,"vars","mappedIcdCode")=icdcode
  . ;
  . ; determine the beginning date and time
  . ;
  . new begindate
- . set begindate=$get(json("entry",zi,"resource","period","start"))
+ . set begindate=$get(@json@("entry",zi,"resource","period","start"))
  . do log(jlog,"beginDateTime is: "_begindate)
- . set eval("careplan",zi,"vars","beginDateTime")=begindate
+ . set @eval@("careplan",zi,"vars","beginDateTime")=begindate
  . new fmBeginDateTime s fmBeginDateTime=$$fhirTfm^SYNFUTL(begindate)
  . d log(jlog,"fileman beginDateTime is: "_fmBeginDateTime)
- . set eval("careplan",zi,"vars","fmBeginDateTime")=fmBeginDateTime ;
+ . set @eval@("careplan",zi,"vars","fmBeginDateTime")=fmBeginDateTime ;
  . new hl7BeginDateTime s hl7BeginDateTime=$$fhirThl7^SYNFUTL(begindate)
  . d log(jlog,"hl7 beginDateTime is: "_hl7BeginDateTime)
- . set eval("careplan",zi,"vars","hl7BeginDateTime")=hl7BeginDateTime ;
+ . set @eval@("careplan",zi,"vars","hl7BeginDateTime")=hl7BeginDateTime ;
  . ;
  . ; determine the end date and time, if any
  . ;
- . new enddate set enddate=$get(json("entry",zi,"resource","period","end"))
+ . new enddate set enddate=$get(@json@("entry",zi,"resource","period","end"))
  . new hl7EndDateTime s hl7EndDateTime=""
  . if enddate'="" d  ;
  . . do log(jlog,"endDateTime is: "_enddate)
- . . set eval("careplan",zi,"vars","endDateTime")=enddate
+ . . set @eval@("careplan",zi,"vars","endDateTime")=enddate
  . . new fmEndDateTime s fmEndDateTime=$$fhirTfm^SYNFUTL(enddate)
  . . d log(jlog,"fileman endDateTime is: "_fmEndDateTime)
- . . set eval("careplan",zi,"vars","fmEndDateTime")=fmEndDateTime ;
+ . . set @eval@("careplan",zi,"vars","fmEndDateTime")=fmEndDateTime ;
  . . s hl7EndDateTime=$$fhirThl7^SYNFUTL(enddate)
  . . d log(jlog,"hl7 endDateTime is: "_hl7EndDateTime)
- . . set eval("careplan",zi,"vars","hl7EndDateTime")=hl7EndDateTime ;
+ . . set @eval@("careplan",zi,"vars","hl7EndDateTime")=hl7EndDateTime ;
  . else  d log(jlog,"no endDateTime")
  . ;
  . ; determine careplan status (active vs inactive)
  . ;
  . n careplanstatus
- . set careplanstatus=$get(json("entry",zi,"resource","status"))
+ . set careplanstatus=$get(@json@("entry",zi,"resource","status"))
  . d log(jlog,"CarePlan Status: "_careplanstatus)
  . n catstr
  . s catstr=sctcode_"~"_cattext_"~"_careplanstatus
@@ -201,16 +197,16 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . d TONOTE^SYNFTIU(ien,encounterId,"  Status: "_careplanstatus)
  . ; determine the encounter visit ien
  . n encounterId
- . s encounterId=$g(json("entry",zi,"resource","context","reference"))
- . i encounterId="" s encounterId=$g(json("entry",zi,"resource","encounter","reference"))
+ . s encounterId=$g(@json@("entry",zi,"resource","context","reference"))
+ . i encounterId="" s encounterId=$g(@json@("entry",zi,"resource","encounter","reference"))
  . ;i encounterId["urn:uuid:" s encounterId=$p(encounterId,"urn:uuid:",2)
- . s eval("careplan",zi,"vars","encounterId")=encounterId
+ . s @eval@("careplan",zi,"vars","encounterId")=encounterId
  . d log(jlog,"reference encounter ID is : "_encounterId)
  . ;
  . ; determine visit ien
  . ;
  . n visitIen s visitIen=$$visitIen^SYNFENC(ien,encounterId)
- . s eval("careplan",zi,"vars","visitIen")=visitIen
+ . s @eval@("careplan",zi,"vars","visitIen")=visitIen
  . d log(jlog,"visit ien is: "_visitIen)
  . ;
  . ; activities
@@ -219,7 +215,7 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . s actien=0
  . s actary=""
  . s actstr=""
- . s an=$na(json("entry",zi,"resource","activity"))
+ . s an=$na(@json@("entry",zi,"resource","activity"))
  . i $d(@an) d TONOTE^SYNFTIU(ien,encounterId,"Care Plan Activities")
  . f  s actien=$o(@an@(actien)) q:+actien=0  d  ;
  . . i actstr'="" s actstr=actstr_"^"
@@ -234,8 +230,8 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . . s actary(actien,"hf")=$$HFACT^SYNFHF($g(actary(actien,"code")),$g(actary(actien,"text")),hfcatien)
  . . d log(jlog,"CP Activity HF: "_$g(actary(actien,"hf")))
  . . s actstr=actstr_"~"_$g(@an@(actien,"detail","status"))
- . m eval("careplan",zi,"vars","activities")=actary
- . s eval("careplan",zi,"vars","actString")=actstr
+ . m @eval@("careplan",zi,"vars","activities")=actary
+ . s @eval@("careplan",zi,"vars","actString")=actstr
  . d log(jlog,"Activity string: "_actstr)
  . ;
  . ; goals
@@ -245,7 +241,7 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . s goalien=0
  . s goalary=""
  . s goalstr=""
- . s gn=$na(json("entry",zi,"resource","goal"))
+ . s gn=$na(@json@("entry",zi,"resource","goal"))
  . n root s root=$$setroot^SYNWD("fhir-intake")
  . n groot s groot=$na(@root@(ien,"json","entry"))
  . f  s goalien=$o(@gn@(goalien)) q:+goalien=0  d  ;
@@ -278,8 +274,8 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . . s goalary(goalien,"hf")=$$HFGOAL^SYNFHF($g(goalary(goalien,"code")),hfcatien,$g(goalary(goalien,"text")),$tr(addcode,"^","-"))
  . . d log(jlog,"CP Goal HF: "_$g(goalary(goalien,"hf")))
  . . s goalstr=goalstr_"~"_$g(@goalroot@("resource","status"))
- . m eval("careplan",zi,"vars","goals")=goalary
- . s eval("careplan",zi,"vars","goalString")=goalstr
+ . m @eval@("careplan",zi,"vars","goals")=goalary
+ . s @eval@("careplan",zi,"vars","goalString")=goalstr
  . d:goalstr'="" log(jlog,"Goal string: "_goalstr)
  . ;
  . ;b
@@ -307,40 +303,40 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . s (DHPPAT,DHPVST,DHPCAT,DHPGOL,DHPSCT,DHPSDT,DHPEDT)="" ;CarePlan update
  . ;
  . s DHPPAT=$$dfn2icn^SYNFUTL(dfn)
- . s eval("careplan",zi,"parms","DHPPAT")=DHPPAT
+ . s @eval@("careplan",zi,"parms","DHPPAT")=DHPPAT
  . ;
  . s DHPVST=visitIen
- . s eval("careplan",zi,"parms","DHPVST")=visitIen
+ . s @eval@("careplan",zi,"parms","DHPVST")=visitIen
  . ;
  . s DHPSCT=$s(addcode'="":+addcode,1:"")
- . s eval("careplan",zi,"parms","DHPSCT")=DHPSCT
+ . s @eval@("careplan",zi,"parms","DHPSCT")=DHPSCT
  . ;
  . s DHPCAT=$g(catstr)
- . s eval("careplan",zi,"parms","DHPCAT")=DHPCAT
+ . s @eval@("careplan",zi,"parms","DHPCAT")=DHPCAT
  . ;
  . s DHPSDT=$g(hl7BeginDateTime)
- . s eval("careplan",zi,"parms","DHPSDT")=DHPSDT
+ . s @eval@("careplan",zi,"parms","DHPSDT")=DHPSDT
  . ;
  . s DHPEDT=$g(hl7EndDateTime)
- . s eval("careplan",zi,"parms","DHPEDT")=DHPEDT
+ . s @eval@("careplan",zi,"parms","DHPEDT")=DHPEDT
  . ;
  . s DHPACT=actstr
- . s eval("careplan",zi,"parms","DHPACT")=DHPACT
+ . s @eval@("careplan",zi,"parms","DHPACT")=DHPACT
  . ;
  . s DHPGOL=goalstr
- . s eval("careplan",zi,"parms","DHPGOL")=DHPGOL
+ . s @eval@("careplan",zi,"parms","DHPGOL")=DHPGOL
  . ;
  . ;s DHPROV=$$MAP^SYNQLDM("OP","provider") ; map should return the NPI number
- . ;s eval("careplan",zi,"parms","DHPROV")=DHPROV
+ . ;s @eval@("careplan",zi,"parms","DHPROV")=DHPROV
  . ;d log(jlog,"Provider NPI for outpatient is: "_DHPROV)
  . ;
  . ;s DHPLOC=$$MAP^SYNQLDM("OP","location")
  . ;n DHPLOCIEN s DHPLOCIEN=$o(^SC("B",DHPLOC,""))
  . ;if DHPLOCIEN="" S DHPLOCIEN=4
- . ;s eval("careplan",zi,"parms","DHPLOC")=DHPLOCIEN
+ . ;s @eval@("careplan",zi,"parms","DHPLOC")=DHPLOCIEN
  . ;d log(jlog,"Location for outpatient is: #"_DHPLOCIEN_" "_DHPLOC)
  . ;
- . s eval("careplan",zi,"status","loadstatus")="readyToLoad"
+ . s @eval@("careplan",zi,"status","loadstatus")="readyToLoad"
  . ;
  . if $g(args("load"))=1 d  ; only load if told to
  . . if $g(ien)'="" if $$loadStatus("careplan",zi,ien)=1 do  quit  ;
@@ -348,29 +344,27 @@ wsIntakeCareplan(args,body,result,ien)        ; web service entry (post)
  . . d  ;  use DATA2PCE
  . . . d log(jlog,"Calling CPLUPDT^SYNDHP91 to add care plans")
  . . . D CPLUPDT^SYNDHP91(.RETSTA,DHPPAT,DHPVST,DHPCAT,DHPACT,DHPGOL,DHPSCT,DHPSDT,DHPEDT) ;
- . . m eval("careplan",zi,"status")=RETSTA
+ . . m @eval@("careplan",zi,"status")=RETSTA
  . . i $g(DEBUG)=1 ZWR RETSTA
  . . d log(jlog,"Return from data loader was: "_$g(RETSTA))
  . . if +$g(RETSTA)=1 do  ;
- . . . s eval("status","loaded")=$g(eval("status","loaded"))+1
- . . . s eval("careplan",zi,"status","loadstatus")="loaded"
+ . . . s @eval@("status","loaded")=$g(@eval@("status","loaded"))+1
+ . . . s @eval@("careplan",zi,"status","loadstatus")="loaded"
  . . else  d  ;
- . . . s eval("status","errors")=$g(eval("status","errors"))+1
- . . . s eval("careplan",zi,"status","loadstatus")="notLoaded"
- . . . s eval("careplan",zi,"status","loadMessage")=$g(RETSTA)
+ . . . s @eval@("status","errors")=$g(@eval@("status","errors"))+1
+ . . . s @eval@("careplan",zi,"status","loadstatus")="notLoaded"
+ . . . s @eval@("careplan",zi,"status","loadMessage")=$g(RETSTA)
  . . n root s root=$$setroot^SYNWD("fhir-intake")
- . . k @root@(ien,"load","careplan",zi)
- . . m @root@(ien,"load","careplan",zi)=eval("careplan",zi)
  ;
  if $get(args("debug"))=1 do  ;
- . m jrslt("source")=json
+ . m jrslt("source")=@json
  . m jrslt("args")=args
- . m jrslt("eval")=eval
- m jrslt("careplanStatus")=eval("careplanStatus")
+ . m jrslt("eval")=@eval
+ m jrslt("careplanStatus")=@eval@("careplanStatus")
  set jrslt("result","status")="ok"
- set jrslt("result","loaded")=$g(eval("status","loaded"))
+ set jrslt("result","loaded")=$g(@eval@("status","loaded"))
  i $g(ien)'="" d  ; called internally
- . m result=eval
+ . ;m result=@eval
  . m result("status")=jrslt("result")
  . m result("dfn")=dfn
  . m result("ien")=ien
